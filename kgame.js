c20f767652ba2631973dc4fadfd8195da88f23e0
François Vieille <vieille.francois@gmail.com>
2018-03-18 20:08:33 +0100

    Code en protoype pour le plaisir :)

--- kgame.js
+++ kgame.js
@@ -1,214 +1,247 @@
 
-// Get the images
-var imgs = [];
 
-$.getJSON("https://spreadsheets.google.com/feeds/list/1neBPd5rdfK2MvMv-iSksqSMRvBJFSjkcSRNBj8sJMko/od6/public/values?alt=json", function(data) {
-  for (var el in data.feed.entry) {
-    imgs.push({
-      key: el,
-      value: data.feed.entry[el].gsx$imgurl.$t +" "+ data.feed.entry[el].gsx$charactername.$t
-    });
+
+// ------- Objet Joeur
+
+var Joueur = function(zoneScorePlayer) {
+	this.score = 0;
+	this.buzz = false;
+	this.authBuzz = true;
+  this.zoneScorePlayer = zoneScorePlayer
+}
+
+Joueur.prototype = {
+	scoreUpdate : function(points) {
+		this.score = this.score + points;
+    this.zoneScorePlayer.textContent = this.score;
+		return this;
+	},
+
+	aBuzze : function(){
+		if(this.authBuzz){
+			this.buzz = true
+		}
+	}
+
+}
+
+
+// ------- Objet Level
+
+var Level = function(level, img) {
+	this.level = level;
+	this.img = img;
+	this.displayedImg = document.getElementById("displayedImg");
+	this.displayedCountDown = document.getElementById("countDown");
+	this.end = 0;
+  this.isPaused = false;
+	this.timeAvailable = 4000;
+  this.timeElapsed = 0;
+
+	this.displayImg();
+  this.interval = this.countDown(this);
+
+}
+
+Level.prototype = {
+
+	displayImg : function() {
+		this.displayedImg.setAttribute('src', this.img);
+		return this;
+	},
+
+	endLevel : function() {
+		this.end = 1;
+		return this;
+	},
+
+	setTimeEnd  : function(){
+		this.timeEnd.setTime(this.now.getTime() + this.timeAvailable);
+		return this;
+	},
+
+	clearInterval : function(){
+		clearInterval(this.interval);
+	},
+
+	displayCountDown : function(text){
+		this.displayedCountDown.innerHTML = text;
+	},
+
+  countDown : function(niveau){
+    x = setInterval(function() {
+      
+      distance = niveau.timeAvailable - niveau.timeElapsed;
+      if(!niveau.isPaused) {
+        niveau.timeElapsed += 10; 
+      }
+
+      var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+      if (distance < 0) {
+        niveau.end = 0;
+        clearInterval(x);
+        niveau.displayCountDown("Vous etes des gros nazes ?");
+      } else {
+        niveau.displayCountDown(seconds);
+      }
+      
+    }, 10)
+
+    return(x)
   }
-  console.log(imgs);
-  return imgs;
-});
+
+}
+
+
+
+
+
+// ------- Objet Partie
+
+
+var Partie = function(divStart) {
+	this.joueur_1 = new Joueur(document.getElementById("scorePlayer1"));
+	this.joueur_2 = new Joueur(document.getElementById("scorePlayer2"));
+  this.joueurHighlight = Array();
+  this.divStart = divStart;
+	this.level = 0;
+  this.niveau = 0;
+	this.spreadsheet = "https://spreadsheets.google.com/feeds/list/1neBPd5rdfK2MvMv-iSksqSMRvBJFSjkcSRNBj8sJMko/od6/public/values?alt=json";
+
+	this.imgs = Array();
+  this.getImgs();
+
+  $(this.divStart).on('click', this.nouveauLevel.bind(this));
+  $(document).on('keydown', this.keypressActions.bind(this));
+}
+
+Partie.prototype = {
+
+	getImgs : function() {
+		var tmp = this.imgs;
+		$.getJSON(this.spreadsheet, function(data) {
+			for (var el in data.feed.entry) {
+				tmp.push({
+					key: el,
+					value: data.feed.entry[el].gsx$imgurl.$t +" "+ data.feed.entry[el].gsx$charactername.$t
+				});
+			}
+			return null;
+		});
+		return this;
+	},
+
+
+	extractImgUrlFromDict : function(){
+		var imgToExtract = this.imgs[this.level].value;
+		var img;
+		img = imgToExtract.substr(0, imgToExtract.indexOf(" ", 0));
+		return img;
+	},
+
+	nouveauLevel : function() {
+
+    this.playersAuthBuzz([this.joueur_1, this.joueur_2], true)
+    this.resethighlightPlayer();
+    this.joueur_1.buzz = this.joueur_2.buzz = false;
+
+    // nouveau level
+    if(this.niveau != 0) {
+      this.niveau.clearInterval();
+    }
+		this.niveau = new Level(this.level, this.extractImgUrlFromDict());
+
+		// fin du level
+		this.level = this.level + 1;
+	},
+
+  highlightPlayer : function(player) {
+    player.zoneScorePlayer.classList.toggle("playerHighlighted");
+    
+    return this;
+  },
+
+  resethighlightPlayer : function(){
+    this.joueurHighlight.forEach(p => this.highlightPlayer(p));
+    this.joueurHighlight = Array();
+    return this;
+  },
+
+  scoreUp : function(player) {
+    player.scoreUpdate(1)
+  },
+
+  playersAuthBuzz : function(players, state){
+    players.forEach(function(p) {
+      p.authBuzz = state;
+    })
+  },
+
+  keypressActions : function(e) {
+    console.log("keypressActions");
+    console.log(e.which);
+
+    switch (e.which) {
+      case 37: // left arrow key // score player 1
+        if(this.niveau.isPaused && this.joueur_1.buzz) {
+          this.scoreUp(this.joueur_1);
+          this.nouveauLevel();
+        }
+      break;
+
+      case 39: // right arrow key // score player 2
+        if(this.niveau.isPaused && this.joueur_2.buzz) {
+          this.scoreUp(this.joueur_2);
+          this.nouveauLevel();
+        }
+      break;
+
+      case 38: // pause
+        this.niveau.isPaused = !this.niveau.isPaused;
+        this.joueur_1.buzz = this.joueur_2.buzz = false;
+        this.playersAuthBuzz([this.joueur_1, this.joueur_2], true);
+        this.resethighlightPlayer();
+      break;
+
+      case 81: // Q key || player 1 buzzer
+        if (!this.niveau.isPaused && this.joueur_1.authBuzz) {
+          this.joueur_1.buzz = true;
+          this.niveau.isPaused = !this.niveau.isPaused
+          this.playersAuthBuzz([this.joueur_1, this.joueur_2], false)
+          this.highlightPlayer(this.joueur_1);
+          this.joueurHighlight = [this.joueur_1]
+        }
+      break;
+
+      case 77: // M key || player 2 buzzer
+        if (!this.niveau.isPaused && this.joueur_2.authBuzz) {
+          this.joueur_2.buzz = true;
+          this.niveau.isPaused = !this.niveau.isPaused
+          this.playersAuthBuzz([this.joueur_1, this.joueur_2], false)
+          this.highlightPlayer(this.joueur_2);
+          this.joueurHighlight = [this.joueur_2];
+        }
+      break;
+
+      case 13: // niveau suivant
+        this.nouveauLevel();
+      break;
+    
+    }
+
+  }
+
+}
+
+
+// Initialisation de la partie
 
 var startGameButton = document.getElementById("startGame");
 var nextGameButton = document.getElementById("nextGame");
 var displayedImg = document.getElementById("displayedImg");
-var imgUrl;
-var level = 0;
 
-function extractImgUrlFromDict(key) {
-  var imgToExtract = imgs[key].value;
-  imgUrl = imgToExtract.substr(0,imgToExtract.indexOf(" ",0));
-  console.log(imgUrl);
-  return imgUrl;
-}
+var P = new Partie(
+  divStart = document.getElementById("startGame")
+);
 
-function displayImg(key) {
-  extractImgUrlFromDict(key);
-  displayedImg.setAttribute("src",imgUrl);
-}
-
-startGameButton.onclick = function() {
-  displayImg(0);
-  level += 1;
-  startCountDown(timeAvailable);
-  return level;
-};
-
-function nextLevel() {
-  level += 1;
-  displayImg(level);
-  return level;
-}
-
-var player1 = document.getElementById("player1");
-var player2 = document.getElementById("player2");
-
-player1.onclick = function() {
-  scoreUp(1);
-  nextLevel();
-}
-
-player2.onclick = function() {
-  scoreUp(2);
-  nextLevel();
-}
-
-
-var scorePlayer1 = 0;
-var scorePlayer2 = 0;
-
-var zoneScorePlayer1 = document.getElementById("scorePlayer1");
-var zoneScorePlayer2 = document.getElementById("scorePlayer2");
-
-
-function scoreUp(player) {
-  if (player == "1") {
-    scorePlayer1 += 1;
-    zoneScorePlayer1.textContent = scorePlayer1;
-    return scorePlayer1;
-  }
-  else if (player == "2") {
-    scorePlayer2 += 1;
-    zoneScorePlayer2.textContent = scorePlayer2;
-    return scorePlayer2;
-  }
-
-};
-
-
-///////////////////////////////////
-
-var isPaused = false;
-
-$(document).keydown(function(e) {
-    switch (e.which) {
-    case 37: //left arrow key // score player 1
-      scoreUp(1);
-      resetBuzzers();
-      highlightPlayer(1);
-      nextLevel();
-      stopCountDown();
-      startCountDown(timeAvailable);
-      break;
-
-    case 39: //right arrow key // score player 2
-      scoreUp(2);
-      resetBuzzers();
-      highlightPlayer(2);
-      nextLevel();
-      stopCountDown();
-      startCountDown(timeAvailable);
-      break;
-
-    case 38: // up arrow key
-      if (!isPaused) {
-        stopCountDown();
-      }
-      else {
-        startCountDown(distance);
-      }
-      break;
-
-    case 81: // Q key || player 1 buzzer
-      if (!isPaused && player1CanBuzz) {
-        isPaused = true;
-        cantBuzz();
-        stopCountDown();
-        highlightPlayer(1);
-      }
-      else {
-      }
-      break;
-
-      case 77: // M key || player 2 buzzer
-        if (!isPaused && player2CanBuzz) {
-          isPaused = true;
-          cantBuzz();
-          stopCountDown();
-          highlightPlayer(2);
-        }
-        else {
-        }
-        break;
-      }
-    // add skip when no one finds that displays something mean
-});
-
-var countDown = document.getElementById("countDown");
-
-var timeAvailable = 4000;
-var distance;
-var x;
-
-function startCountDown(t) {
-  isPaused = false;
-  // reset distance
-  // distance = timeAvailable;
-
-  // Set the date we're counting down to
-  var d = new Date();
-  var countDownDate = d.setTime(d.getTime() + t);
-
-  // Update the count down every 10 milliseconds
-  x = setInterval(function() {
-
-  // Get todays date and time
-  var now = new Date().getTime();
-
-  // Find the distance between now an the count down date
-
-  distance = countDownDate - now;
-
-  // Time calculations for days, hours, minutes and seconds
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-  var milliseconds = distance;
-
-  // Display the result in the element
-  document.getElementById("countDown").innerHTML = seconds;
-
-  // If the count down is finished, write some text
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("countDown").innerHTML = "VOUS ÊTES NAZES";
-    distance = 0;
-    nextLevel();
-    startCountDown(timeAvailable);
-    }
-  }, 10);
-}
-
-function stopCountDown() {
-  clearInterval(x);
-  isPaused = true;
-  return distance;
-}
-
-function skipLevel() {
-
-}
-
-
-
-// BUZZER
-
-function highlightPlayer(n) {
-  var playerToHighlight = document.getElementById("zonePlayer"+n);
-  playerToHighlight.classList.toggle("playerHighlighted");
-}
-
-var player1CanBuzz = true;
-var player2CanBuzz = true;
-
-function resetBuzzers() {
-  player1CanBuzz = true;
-  player2CanBuzz = true;
-}
-
-function cantBuzz() {
-  player1CanBuzz = false;
-  player2CanBuzz = false;
-}
